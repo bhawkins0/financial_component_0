83262140b5f8e04df30ff32e602a8917507a0f72
bhawkins0 <bhawkin0@chicagobooth.edu>
2021-03-07 05:05:54 +0000

    before_env_vars

--- app/controllers/plaid_link_controller.rb
+++ app/controllers/plaid_link_controller.rb
@@ -2,19 +2,19 @@
 class PlaidLinkController < ApplicationController
   def index
     client = Plaid::Client.new(env: 'sandbox',
-      client_id: '5eb39ba40b2dcc00124d076f',  
-      secret: '8405e8cc1b8fcc62af5b3fc1819ebc')
-      #client_id: ENV['PLAID_CLIENT_ID'],
-      #secret: ENV['PLAID_SECRET'])
+      #client_id: '5eb39ba40b2dcc00124d076f',  
+      #secret: '8405e8cc1b8fcc62af5b3fc1819ebc')
+      client_id: ENV['PLAID_CLIENT_ID'],
+      secret: ENV['PLAID_SECRET'])
 
     create_response = client.sandbox.sandbox_public_token.create(
-      institution_id: SANDBOX_INSTITUTION,
+      institution_id: "ins_1",
       initial_products: ['transactions']
     )
 
     # The generated public_token can now be
     # exchanged for an access_token
-    publicToken = response.public_token
+    publicToken = create_response.public_token
     exchange_token_response = client.item.public_token.exchange(publicToken)
 
     @access_token = exchange_token_response.fetch("access_token")
