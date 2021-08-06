4b02fd682b60b97f106e9d966977db418d451960
bhawkins0 <bhawkin0@chicagobooth.edu>
2021-08-06 17:14:04 +0000

    pre_push_changes_5

--- db/migrate/20210324000836_change_financial_component_keyword_column.rb
+++ db/migrate/20210324000836_change_financial_component_keyword_column.rb
@@ -1,6 +1,6 @@
 class ChangeFinancialComponentKeywordColumn < ActiveRecord::Migration[6.0]
   def change
-    change_column :financial_component_keywords, :fc_debit, :integer
-    change_column :financial_component_keywords, :fc_credit, :integer
+    change_column :financial_component_keywords, :fc_debit, "integer USING NULLIF(fc_debit, '')::int"
+    change_column :financial_component_keywords, :fc_credit, "integer USING NULLIF(fc_credit, '')::int"r
   end
 end
