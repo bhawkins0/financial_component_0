6e35a6e37392b4016afbd33acd74c9cceeb38946
bhawkins0 <bhawkin0@chicagobooth.edu>
2021-09-09 03:30:51 +0000

    user_registration_progress

--- app/controllers/user_authentication_controller.rb
+++ app/controllers/user_authentication_controller.rb
@@ -1,6 +1,6 @@
 class UserAuthenticationController < ApplicationController
   # Uncomment this if you want to force users to sign in before any other actions
-  skip_before_action(:force_user_sign_in, { :only => [:sign_up_form, :create, :sign_in_form, :create_cookie, :process_login_form, :reset_password, :validate_password_reset, :validate_email, :validate_mobile_for_edit_trigger, :validate_mobile_code_for_edit_trigger, :update_password, :about, :contact] })
+  skip_before_action(:force_user_sign_in, { :only => [:sign_up_form, :create, :sign_in_form, :create_cookie, :process_login_form, :reset_password, :validate_password_reset, :validate_email, :validate_mobile, :validate_mobile_code, :update_password, :about, :contact] })
 
   def process_login_form
     a = params.fetch("subject")
@@ -58,20 +58,20 @@ class UserAuthenticationController < ApplicationController
 
   def create
     @user = User.new
-    @user.email = params.fetch("userauthentication").fetch("query_email")
-    @user.password = params.fetch("userauthentication").fetch("query_password")
-    @user.password_confirmation = params.fetch("userauthentication").fetch("query_password_confirmation")
-    @user.first_name = params.fetch("userauthentication").fetch("query_first_name")
-    @user.last_name = params.fetch("userauthentication").fetch("query_last_name")
+    @user.email = params.fetch("query_email")
+    @user.password = params.fetch("query_password")
+    @user.password_confirmation = params.fetch("query_password_confirmation")
+    @user.first_name = params.fetch("query_first_name")
+    @user.last_name = params.fetch("query_last_name")
 
     save_status = @user.save
 
     if save_status == true
       session[:user_id] = @user.id
    
-      redirect_to("/index", { :notice => "User account created successfully."})
-    else
-      redirect_to("/sign_up", { :alert => "User account failed to create successfully."})
+      #redirect_to("/index", { :notice => "User account created successfully."})
+      #else
+      #redirect_to("/sign_up", { :alert => "User account failed to create successfully."})
     end
   end
     
@@ -111,36 +111,35 @@ class UserAuthenticationController < ApplicationController
     render({ :template => "user_authentication/add_mobile.html.erb" })
   end
 
-  def validate_mobile_trigger
-    validate_mobile
+  def validate_email
+    email_exists = User.where({ :email => params.fetch("query_email") }).first
 
-    @verification_stage = 1
-
-    respond_to do |format|
-      format.js
+    if email_exists == nil
+      @email_exists = 1
+    else 
+      @email_exists = 2
+      session[:email] = params.fetch("query_email")
     end
-  end
+    
+    flags = params.fetch("flags").to_i
+    @flags = flags
+    
+    if flags == 1
+      if email_exists == nil 
+        create
 
-  def validate_mobile_code_trigger
-    @twilio_status = validate_mobile_code
+        email_code
 
-    respond_to do |format|
-      format.js
+        user_verification = UserVerification.new
+        user_verification.user_id =  @user.id
+        user_verification.email = params.fetch("query_email")
+        user_verification.email_validation_code = session[:email_code]
+
+        if user_verification.valid?
+          user_verification.save
+        end
+      end
     end
-  end
-
-  def validate_mobile_for_edit_trigger
-    validate_mobile
-
-    @verification_stage = 1
-
-    respond_to do |format|
-      format.js
-    end
-  end
-
-  def validate_mobile_code_for_edit_trigger
-    @twilio_status = validate_mobile_code
 
     respond_to do |format|
       format.js
@@ -149,6 +148,7 @@ class UserAuthenticationController < ApplicationController
 
   def validate_mobile
     recipient = params.fetch("recipient")
+    @flags = params.fetch("flags").to_i
     # Your Account Sid and Auth Token from twilio.com/console
     # and set the environment variables. See http://twil.io/secure
     account_sid = ENV['TWILIO_ACCOUNT_SID']
@@ -168,6 +168,15 @@ class UserAuthenticationController < ApplicationController
 
     session.store(:twilio_sid, verification.sid)
     session.store(:twilio_service_sid, verification.service_sid)
+
+    @verification_stage = 1
+
+    user_verification = User.where({ :id => session[:user_id] }).first
+    user_verification.mobile = recipient
+
+    respond_to do |format|
+      format.js
+    end
   end
 
   def validate_mobile_code
@@ -184,60 +193,54 @@ class UserAuthenticationController < ApplicationController
       @twilio_status = 'approved'
       phone_number = verification_check.to
       #p phone_number
-      flags = params.fetch("flags").to_i
-      if flags != 2
+      @flags = params.fetch("flags").to_i
+      if @flags == 0
         @current_user.mobile = phone_number
+        if @current_user.valid?
+          @current_user.save
+        end
+      elsif @flags == 1
+        user = User.where({ :id => session[:user_id] }).first
+        user.mobile = phone_number
+        user.mobile_verified_at = DateTime.now
         if @current_user.valid?
           @current_user.save
         end
       end
     end
 
-    return @twilio_status
-  end
-
-  def validate_email
-    email_exists = User.where({ :email => params.fetch("email") }).first
-
-    if email_exists == nil
-      @email_exists = 1
-    else 
-      @email_exists = 2
-      session[:email] = params.fetch("email")
-    end
-    
-    flags = params.fetch("flags").to_i
-    @flags = flags
-    
     respond_to do |format|
       format.js
     end
   end
 
   include SendGrid
-  def reset_password
+  def email_code
     # using SendGrid's Ruby Library
     # https://github.com/sendgrid/sendgrid-ruby
 
-    session[:vCode] = rand(1000..9999)
+    session[:email_code] = rand(1000..9999)
     
-    #p session[:vCode]
     from = Email.new(email: 'bhawkins2012@gmail.com')
     to = Email.new(email: session[:email])
-    subject = 'Financial Component Password Reset'
-    content = Content.new(type: 'text/plain', value: 'Please see below for a validation code to reset your password to Financial Component. If you did not make this request, please reach out to us immediately. Thank you.' + "\n" + "\n" + session[:vCode].to_s)
+    if @flags == 0
+      subject = 'Financial Component Password Reset'
+      content = Content.new(type: 'text/plain', value: 'Please see below for a validation code to reset your password to Financial Component. If you did not make this request, please reach out to us immediately. Thank you.' + "\n" + "\n" + session[:email_code].to_s)
+    elsif @flags == 1
+      subject = 'Financial Component Email Verification'
+      content = Content.new(type: 'text/plain', value: 'Please see below for a code to verify your email with Financial Component. If you did not make this request, please reach out to us immediately. Thank you.' + "\n" + "\n" + session[:email_code].to_s)
+    end
     mail = Mail.new(from, subject, to, content)
 
     sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
     response = sg.client.mail._('send').post(request_body: mail.to_json)
-    #puts response.status_code
-    #puts response.body
-    #puts response.headers
     
-    render({ :template => "user_authentication/reset_password.html.erb" })
+    if @flags == 0
+      render({ :template => "user_authentication/reset_password.html.erb" })
+    end
   end
 
-  def validate_password_reset
+  def validate_email_code
     @verification_stage = 1
 
     if params.fetch("code") == session[:vCode].to_s
