fc5033947071860374fcb75be6b2ff64383f05b4
bhawkins0 <bhawkin0@chicagobooth.edu>
2022-06-12 18:01:24 +0000

    user_verification_comment

--- db/migrate/20210921040804_change_user_verification.rb
+++ db/migrate/20210921040804_change_user_verification.rb
@@ -1,9 +1,9 @@
-class ChangeUserVerification < ActiveRecord::Migration[6.0]
-    def up
-      change_column :user_verifications, :mobile_validation_code, :datetime
-    end
+#class ChangeUserVerification < ActiveRecord::Migration[6.0]
+#    def up
+#      change_column :user_verifications, :mobile_validation_code, :datetime
+#    end
 
-    def down
-      change_column :user_verifications, :mobile_validation_code, :mobile_verified_at
-    end
-end
+#    def down
+#      change_column :user_verifications, :mobile_validation_code, :mobile_verified_at
+#    end
+#end
