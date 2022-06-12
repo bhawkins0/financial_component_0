1a4a9b3997b1add078ceaa01fffb8b711854ab72
bhawkins0 <bhawkin0@chicagobooth.edu>
2022-06-12 17:49:27 +0000

    updated_migration

--- db/migrate/20210909000911_add_email_verified_at_to_users.rb
+++ db/migrate/20210909000911_add_email_verified_at_to_users.rb
@@ -1,5 +1,5 @@
 class AddEmailVerifiedAtToUsers < ActiveRecord::Migration[6.0]
   def change
-    add_column :users, :email_verified_at, :datetime
+    add_column :users, :email_verified_at, :DateTime
   end
 end
